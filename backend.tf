679b56ab69d25e0a6c36187684602c762f7020f5
Patrick Mbaza <patrickmbaza@gmail.com>
2025-02-05 11:33:16 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
   backend "s3" {
     bucket         = "my-tf-state-bucket2122"
     key            = "path/terraform.tfstate"
-    region         = "eu-west-2"
+    region         = "us-east-1"
   }
 }
